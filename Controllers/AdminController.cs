533adb2d617d2243f7f1b89402c7a55540cebd2e
Ramunė Alionytė <66987985+celly666@users.noreply.github.com>
2021-04-16 20:13:47 +0300

    Web API Project using Visual Studio 2019 with SQL

---
+++ Controllers/AdminController.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using WebApiAPP.Models;
+
+namespace WebApiAPP.Controllers
+{
+    public class AdminController : Controller
+    {
+        private duombazeEntities db = new duombazeEntities();
+
+        // GET: Admin
+        public ActionResult Index()
+        {
+            return View(db.Admins.ToList());
+        }
+
+        // GET: Admin/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Admin admin = db.Admins.Find(id);
+            if (admin == null)
+            {
+                return HttpNotFound();
+            }
+            return View(admin);
+        }
+
+        // GET: Admin/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Admin/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "id,name,email,password,contact")] Admin admin)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Admins.Add(admin);
+                db.SaveChanges();
+                TempData["SuccessMessage"] = "New record save successfully";
+                return RedirectToAction("Index");
+            }
+
+            return View(admin);
+        }
+
+        // GET: Admin/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Admin admin = db.Admins.Find(id);
+            if (admin == null)
+            {
+                return HttpNotFound();
+            }
+            return View(admin);
+        }
+
+        // POST: Admin/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "id,name,email,password,contact")] Admin admin)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(admin).State = EntityState.Modified;
+                db.SaveChanges();
+                TempData["SuccessMessage"] = "Record edited successfully";
+                return RedirectToAction("Index");
+            }
+            return View(admin);
+        }
+
+        // GET: Admin/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Admin admin = db.Admins.Find(id);
+            if (admin == null)
+            {
+                return HttpNotFound();
+            }
+            return View(admin);
+        }
+
+        // POST: Admin/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Admin admin = db.Admins.Find(id);
+            db.Admins.Remove(admin);
+            db.SaveChanges();
+            TempData["SuccessMessage"] = "Record deleted successfully";
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ Controllers/NovelController.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using WebApiAPP.Models;
+
+namespace WebApiAPP.Controllers
+{
+    public class NovelController : Controller
+    {
+        private duombazeEntities db = new duombazeEntities();
+
+        // GET: Novel
+        public ActionResult Index()
+        {
+            return View(db.Novels.ToList());
+        }
+
+        // GET: Novel/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Novel novel = db.Novels.Find(id);
+            if (novel == null)
+            {
+                return HttpNotFound();
+            }
+            return View(novel);
+        }
+
+        // GET: Novel/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Novel/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "id,bookName,Author,Description")] Novel novel)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Novels.Add(novel);
+                db.SaveChanges();
+                TempData["SuccessMessage"] = "Record save successfully";
+                return RedirectToAction("Index");
+            }
+
+            return View(novel);
+        }
+
+        // GET: Novel/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Novel novel = db.Novels.Find(id);
+            if (novel == null)
+            {
+                return HttpNotFound();
+            }
+            return View(novel);
+        }
+
+        // POST: Novel/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "id,bookName,Author,Description")] Novel novel)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(novel).State = EntityState.Modified;
+                db.SaveChanges();
+                TempData["SuccessMessage"] = "Record edited successfully";
+                return RedirectToAction("Index");
+            }
+            return View(novel);
+        }
+
+        // GET: Novel/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Novel novel = db.Novels.Find(id);
+            if (novel == null)
+            {
+                return HttpNotFound();
+            }
+            return View(novel);
+        }
+
+        // POST: Novel/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Novel novel = db.Novels.Find(id);
+            db.Novels.Remove(novel);
+            db.SaveChanges();
+            TempData["SuccessMessage"] = "Record deleted successfully";
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
